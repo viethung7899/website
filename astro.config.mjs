3660dc2fb24286107b961b5d21dca86eccc1520d
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-02 11:23:28 -0700

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://vhn.vercel.app",
   integrations: [react(), tailwind({
     applyBaseStyles: false,
   })]
