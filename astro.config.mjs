b6c13609e24331b0aa4389d438466e33cfe70224
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-16 00:26:09 -0700

    Add React Email

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vhn.vercel.app",
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ],
   output: "hybrid",
   adapter: vercel()
-});
+})
