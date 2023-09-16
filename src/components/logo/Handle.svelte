54b70a023f8b8f42765702ead75feeb44627ec4f
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-16 05:54:00 -0700

    Fancy logo animation

---
+++ src/components/logo/Handle.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { cn } from "@/lib/utils"
+  import type { ClassValue } from "clsx"
+  import {scale} from "svelte/transition";
+  export let show = true;
+  export let className: ClassValue | undefined;
+</script>
+
+<span transition:scale class={cn(
+  "transition-all duration-500 ease-in-out",
+  show ? "opacity-100" : "opacity-0",
+  show ? undefined : (className || "mr-[-0.5em]"),
+)}>
+  <slot />
+</span>
---
+++ src/components/logo/Logo.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Handle from "./Handle.svelte";
+  let scrollY = 0;
+
+  $: isTop = scrollY <= 0;
+</script>
+
+<svelte:window bind:scrollY={scrollY} />
+<a href="/" class="font-mono text-primary">
+  <h2 class="inline-flex">
+    <Handle show={!isTop}>&lsqb;</Handle>
+    <span>v</span>
+    <Handle show={isTop} className="mr-[-2.5em]">iet-</Handle>
+    <span>h</span>
+    <Handle show={isTop} className="mr-[-2.5em]">ung&nbsp;</Handle>
+    <span>ng</span>
+    <Handle show={isTop} className="mr-[-2.5em]">uyen</Handle>
+    <Handle show={!isTop}>&rsqb;</Handle>
+  </h2>
+</a>
