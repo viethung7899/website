1afe85e6fbc38a5f489923967e84426b7d2eaa41
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-15 23:01:37 -0700

    Add Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config"
-import react from "@astrojs/react"
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vhn.vercel.app",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false
-    })
-  ]
-})
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "hybrid",
+  adapter: vercel()
+});
