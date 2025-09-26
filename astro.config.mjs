be63a89c0b7eae80447290a8233eafe3007afdef
Viet Hung Nguyen <vhn@sfu.ca>
2025-09-26 14:15:02 -0700

    Upgrade to Astro v5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import react from "@astrojs/react"
 import tailwind from "@tailwindcss/vite"
-import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vhng.dev",
-  integrations: [
-    react(),
-  ],
-  output: "hybrid",
+  integrations: [react()],
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true
