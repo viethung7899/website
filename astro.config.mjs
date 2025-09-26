ca640878538ba7f48f69d4e64091ac5bb78d8855
Viet Hung Nguyen <vhn@sfu.ca>
2025-09-26 14:11:43 -0700

    Upgrade to Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
 import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@tailwindcss/vite"
 import vercel from "@astrojs/vercel/serverless"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vhn.vercel.app",
+  site: "https://vhng.dev",
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false
-    })
   ],
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true
     }
-  })
+  }),
+  vite: {
+    plugins: [tailwind()]
+  }
 })
